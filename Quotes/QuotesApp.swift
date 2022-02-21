a76a7b2966c1a1010849abd2a005957c44e74a05
Luis Calcano <luiscalcano@me.com>
2022-02-21 12:45:10 -0400

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Luis Calcano on 21/2/22.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
